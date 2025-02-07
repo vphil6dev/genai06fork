53c672edcdf151e8865801cf5b16d2f352c032f6
Philippe Duvivier <ph.duvivier@gmail.com>
2025-02-07 16:04:34 +0000

    openai completion

--- challenge/index.js
+++ challenge/index.js
@@ -1,11 +1,17 @@
-require('dotenv').config();
-const OpenAI = require('openai');
+//require('dotenv').config();
+//const OpenAI = require('openai');
+import { configDotenv } from 'dotenv';
+import OpenAI from 'openai/index.mjs';
 const readlineSync = require('readline-sync');
 
 // Open AI configuration
-const openai = new OpenAI({
-  apiKey: "",
-});
+/* const openai = new OpenAI({
+  apiKey: process.env.GENAICURSUSKEY,
+}); */
+/* const openai = new OpenAI({
+  apiKey: configDotenv({ DOTENV_KEY: 'GENAICURSUSKEY'})
+}); */
+const openai = new OpenAI();
 
 // Get user input
 function getInput(promptMessage) {
@@ -24,7 +30,7 @@ async function main() {
 
 async function runConversation() {
 
-  let messages = [{ role: "system", content: "You are a funny assistant." }];
+  //let messages = [{ role: "system", content: "You are a funny assistant." }];
   //let messages = [{ role: "system", content: "You are a helpful assistant." }];
 
   while (true) {
@@ -33,13 +39,30 @@ async function runConversation() {
       console.log("Goodbye!");
       process.exit();
     }
-    messages.push({ "role": "user", "content": input });
+
+    // EXAMPLE PROVENANT DE L'EXERCICE PRÉCÉDENT
+    /* messages.push({ "role": "user", "content": input });
     const completion = await openai.chat.completions.create({
       model: "gpt-4o-mini",
       messages: messages,
     });
     console.log(completion.choices[0].message.content);
-    // messages.push(completion.choices[0].message.content);
+    // messages.push(completion.choices[0].message.content); */
+
+    // EXAMPLE PROVENANT DU SITE OPENAI
+    const completion = await openai.chat.completions.create({
+      model: "gpt-4o-mini",
+      messages: [
+        { role: "system", content: "You are a helpful assistant." },
+        {
+          role: "user",
+          content: "Write a haiku about recursion in programming.",
+        },
+      ],
+      store: true,
+    });
+    console.log(completion.choices[0].message);
+
   }
 }
 
